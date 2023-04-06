5150a1841184494883d5ee5c4d197a53084ab212
HerrSchmieroel <121556288+HerrSchmieroel@users.noreply.github.com>
2023-04-06 19:45:54 +0200

    ...

---
+++ linktree/index.js
@@ -0,0 +1,9 @@
+document.getElementById("cpstester").onclick = function() {
+    window.location.href = "https://herrschmieroel.github.io/cps-tester/index.html";
+}
+document.getElementById("ohiomarket").onclick = function() {
+    window.location.href = "https://herrschmieroel.github.io/ohiomarket/index.html";
+}
+document.getElementById("calculator").onclick = function() {
+    window.location.href = "https://herrschmieroel.github.io/Calculator/ENG/index.html";
+}
