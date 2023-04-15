79ef1628aea7835ac1418fe77392d7e23ba0942c
HerrSchmieroel <121556288+HerrSchmieroel@users.noreply.github.com>
2023-04-15 18:59:39 +0200

    I hate merge conflicts

--- linktree/discordbots/bot.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("Schmieroel").onclick = function() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1094275590007296203&permissions=414464723968&scope=bot";
-}
