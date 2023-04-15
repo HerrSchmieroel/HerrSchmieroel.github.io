d6f4225a007d0472b1f8057a3b18453cc06a97e7
HerrSchmieroel <121556288+HerrSchmieroel@users.noreply.github.com>
2023-04-15 18:54:49 +0200

    ...

--- linktree/index.js
+++ linktree/index.js
@@ -7,14 +7,20 @@ document.getElementById("ohiomarket").onclick = function() {
 document.getElementById("calculator").onclick = function() {
     window.location.href = "https://herrschmieroel.github.io/Calculator/ENG/index.html";
 }
+
 document.getElementById("rock_paper_scissors").onclick = function() {
     window.location.href = "https://herrschmieroel.github.io/Rock_Paper_Scissors/index.html";
-}
-
-document.getElementById("discordbots").onclick = function() {
-    window.location.href = "discordbots/bot.html";
 }
 
 document.getElementById("github").onclick = function() {
     window.location.href = "https://github.com/HerrSchmieroel";
 }
+document.getElementById("discordbots").onclick = function() {
+    window.location.href = "discordbots/bot.html";
+}
+
+//window.addEventListener('load', function() {
+//    var audio = document.getElementById('bg-music');
+//    audio.play();
+//  });
+
